10b0fafbf15fe1c54e0642233164c1c54497a5e0
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-04-26 11:58:37 +0200

    final version ready to hosting

---
+++ routes/api.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const db = 'mongodb://userNicolas:userNicolas@ds229909.mlab.com:29909/eventsbd';
+mongoose.connect(db, err =>{
+    if(err){
+        console.error('ERROR!' + err);
+    } else{
+        console.log('Connected to MongoDB');
+    };
+});
+function verifyToken(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(401).send('Unauthorazid request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null'){
+        return res.status(401).send('Unauthorazid request')
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if (!payload){
+        return res.status(401).send('Unauthorazid request')
+    }
+    req.userId = payload.subject
+    next()
+};
+router.get('/', (req, res) => {
+    res.send('from API route');
+});
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser)=>{
+        if(error){
+            console.log(error);
+        }else{
+            let payload = {subject: registeredUser._id};
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({token});
+        };
+    });
+});
+router.post('/login', (req, res) => {
+    let userData = req.body;
+    User.findOne({email: userData.email}, (error, user)=>{
+        if(error){
+            console.log(error);
+        }else{
+            if(!user){
+                res.status(401).send('Invalid email')
+            }else if(user.password !== userData.password){
+                res.status(401).send('Invalid password')
+            }else{
+                let payload = {subject: user._id};
+                let token = jwt.sign(payload, 'secretKey');
+                res.status(200).send({token})
+            };
+        };
+    });
+});
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        }
+    ];
+    res.json(events);
+});
+router.get('/special', verifyToken, (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        },
+        {
+            "_id": "6",
+            "name": "JSmeet",
+            "description": "lorem insup",
+            "date": "2012-04-23T10:30:00.511Z"
+        }
+    ];
+    res.json(events);
+});
+
+module.exports = router;
